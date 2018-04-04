bd2d9e7d88e11f28f17c45c4b1665ad85d796f4f
root <root@gdgsurabaya.org>
2018-04-04 02:10:40 +0000

    initial commit

---
+++ run.py
@@ -0,0 +1,20 @@
+import falcon
+
+from falcon_cors import CORS
+
+from lib.middleware.json_translator import JSONTranslator
+from lib.middleware.require_json import RequireJSON
+from lib.listener.play import PlayListener
+from lib.listener.kuis import KuisListener
+from lib.listener.display import DisplayListener
+
+cors = CORS(allow_all_origins=True, allow_all_headers=True, allow_all_methods=True)
+app  = falcon.API(middleware=[
+  RequireJSON(), 
+  JSONTranslator(),
+  cors.middleware
+])
+
+app.add_route("/kuis", KuisListener())
+app.add_route("/play/{kuis_id}", PlayListener())
+app.add_route("/display/{kuis_id}/{user_id}", DisplayListener())
