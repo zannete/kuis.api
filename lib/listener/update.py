d0e82d8dbb6067735a6eae6e4d2f232b5be6aa8a
michael fernando <michaelfernando784@gmail.com>
2018-04-06 13:53:07 +0700

    Add files via upload

--- lib/listener/update.py
+++ lib/listener/update.py
@@ -1,6 +1,7 @@
 import urllib
 import hashlib
 import os
+import random
 
 import falcon
 import requests
@@ -13,7 +14,7 @@ from firebase_admin import db
 
 class UpdateListener:
 
-    def on_patch(self, req, res):
+    def on_patch(self, req, res, kuis_id):
 
         doc            = req.context["doc"]
         judul          = doc["judul"]
@@ -21,26 +22,19 @@ class UpdateListener:
         profile_y      = doc["profileY"]
         profile_width  = doc["profileWidth"]
         profile_height = doc["profileHeight"]
-        cover_image    = doc["coverImage"]
-        list_jawaban   = doc["listJawaban"]
 
         try:
-            cred         = credentials.Certificate(os.path.join(os.getcwd(), "serviceAccountKey.json"))
+            cred         = credentials.Certificate(os.path.join(os.getcwd(), "service.json"))
             firebase_app = firebase_admin.initialize_app(cred, {
-                'storageBucket': 'kuis.zannete.com',
-                'databaseURL': 'https://kuis-zannete.firebaseio.com/'
+                'databaseURL': 'https://coba-d5c28.firebaseio.com//'
             })
         except ValueError:
             print("Firebase has been initialized")
-        bucket = storage.bucket()
 
-        kuis_ref = db.reference("/kuis")
+        kuis_ref = db.reference("/kuis/{}".format(kuis_id))
 
         kuis_ref.set({"judul":judul})
         kuis_ref.child("profileX").set({"profileX":profile_x})
-        kuis_ref.child("profileY").set({"profileY":profile_y})
-        kuis_ref.child("profileWidth").set({"profileWidth":profile_width})
-        kuis_ref.child("profileHeight").set({"profileHeight":profile_height})
-        kuis_ref.child("listJawaban").set({"listJawaban":url_list_jawaban})
+        kuis_ref.child("profileY").set({"profileY":profile_y}) kuis_ref.child("profileWidth").set({"profileWidth":profile_width}) kuis_ref.child("profileHeight").set({"profileHeight":profile_height})
 
         req.context["result"] = {"status": {"code": 200, "message": "success"}}
