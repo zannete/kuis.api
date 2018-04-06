52c699608fe06c02b226d77bcb6b4d30c50ac502
michael fernando <michaelfernando784@gmail.com>
2018-04-06 11:50:27 +0700

    Add files via upload

--- lib/listener/update.py
+++ lib/listener/update.py
@@ -12,68 +12,57 @@ from firebase_admin import storage
 from firebase_admin import db
 
 
-class UpdateListener(object):
+class UpdateListener:
 
-  def save_image(self,bucket,folder,image_data):
-      image_data   = image_data.split("data:image/jpeg;base64,")[1]
-      n_image_data = "{}.jpg".format(hashlib.sha256(image_data.encode("utf-8")).hexdigest())
-      f_image_data = open(os.path.join(os.getcwd(), "tmpImage", n_image_data), "wb")
-      f_image_data.write(base64.decodestring(image_data.encode("utf-8")))
-      f_image_data.close()
+    def save_image(self,bucket,folder,image_data):
+        image_data   = image_data.split("data:image/jpeg;base64,")[1]
+        n_image_data = "{}.jpg".format(hashlib.sha256(image_data.encode("utf-8")).hexdigest())
+        f_image_data = open(os.path.join(os.getcwd(), "tmpImage", n_image_data), "wb")
+        f_image_data.write(base64.decodestring(image_data.encode("utf-8")))
+        f_image_data.close()
 
-      b_image_data = bucket.blob("{}/{}".format(folder, n_image_data))
-      b_image_data.upload_from_file(open(os.path.join(os.getcwd(), "tmpImage", n_image_data), "rb"), content_type="image/jpeg")
-      b_image_data.make_public()
+        b_image_data = bucket.blob("{}/{}".format(folder, n_image_data))
+        b_image_data.upload_from_file(open(os.path.join(os.getcwd(), "tmpImage", n_image_data), "rb"), content_type="image/jpeg")
+        b_image_data.make_public()
 
-      os.remove(os.path.join(os.getcwd(), "tmpImage", n_image_data))
-      return b_image_data.public_url
+        os.remove(os.path.join(os.getcwd(), "tmpImage", n_image_data))
+        return b_image_data.public_url
 
-  def on_patch(self, req, res, kuis_id):
+    def on_patch(self, req, res):
 
-    doc            = req.context["doc"]
-    judul          = doc["judul"]
-    profile_x      = doc["profileX"]
-    profile_y      = doc["profileY"]
-    profile_width  = doc["profileWidth"]
-    profile_height = doc["profileHeight"]
-    cover_image    = doc["coverImage"]
-    list_jawaban   = doc["listJawaban"]
+        doc            = req.context["doc"]
+        judul          = doc["judul"]
+        profile_x      = doc["profileX"]
+        profile_y      = doc["profileY"]
+        profile_width  = doc["profileWidth"]
+        profile_height = doc["profileHeight"]
+        cover_image    = doc["coverImage"]
+        list_jawaban   = doc["listJawaban"]
 
-    try:
-      cred         = credentials.Certificate(os.path.join(os.getcwd(), "serviceAccountKey.json"))
-      firebase_app = firebase_admin.initialize_app(cred, {
-        'storageBucket': 'kuis.zannete.com',
-        'databaseURL': 'https://kuis-zannete.firebaseio.com/'
-      })
-    except ValueError:
-      print("Firebase has been initialized")
-      bucket = storage.bucket()
+        try:
+            cred         = credentials.Certificate(os.path.join(os.getcwd(), "serviceAccountKey.json"))
+            firebase_app = firebase_admin.initialize_app(cred, {
+                'storageBucket': 'kuis.zannete.com',
+                'databaseURL': 'https://kuis-zannete.firebaseio.com/'
+            })
+        except ValueError:
+            print("Firebase has been initialized")
+        bucket = storage.bucket()
 
-    kuis_ref = db.reference("/jawaban/kuis/{}".format(kuis_id))
+        kuis_ref = db.reference("/kuis")
 
-    cek = input("cek Id: ")
+        kuis_ref.set({"judul":judul})
+        url_cover_image = self.save_image(bucket, "coverImage", cover_image)
+        kuis_ref.set({"coverImage":cover_image})
+        kuis_ref.child("profileX").set({"profileX":profile_x})
+        kuis_ref.child("profileY").set({"profileY":profile_y})
+        kuis_ref.child("profileWidth").set({"profileWidth":profile_width})
+        kuis_ref.child("profileHeight").set({"profileHeight":profile_height})
+        update_list_jawaban = input("update_list_jawaban: ")
+        url_list_jawaban = []
+        for jawaban in list_jawaban:
+            url_jawaban = self.save_image(bucket, "jawabanImage", jawaban)
+            url_list_jawaban.append(url_jawaban)
+        kuis_ref.child("listJawaban").set({"listJawaban":url_list_jawaban})
 
-    if cek in kuis_ref.child(kuis_id).get().values():
-      update_judul = input("Masukkan Judul: ")
-      kuis_ref.set({"judul":update_judul})
-      url_cover_image = self.save_image(bucket, "coverImage", cover_image)
-      kuis_ref.set({"coverImage":url_cover_image})
-      update_profile_x = input("Masukkan Profile X: ")
-      kuis_ref.child("profileX").set({"profileX":update_profile_x})
-      update_profile_y = input("Masukkan Profile Y: ")
-      kuis_ref.child("profileY").set({"profileY":update_profile_y})
-      update_profile_width = input("Masukkan Profile width: ")
-      kuis_ref.child("profileWidth").set({"profileWidth":update_profile_width})
-      update_profile_height = input("Masukkan Profile height: ")
-      kuis_ref.child("profileHeight").set({"profileHeight":update_profile_height})
-      update_list_jawaban = input("update_list_jawaban: ")
-      url_list_jawaban = []
-      for jawaban in update_list_jawaban:
-          url_jawaban = self.save_image(bucket, "jawabanImage", jawaban)
-          url_list_jawaban.append(url_jawaban)
-      kuis_ref.child("listJawaban").set({"listJawaban":url_list_jawaban})
-
-      req.context["result"] = {"status": {"code": 200, "message": "success"}}
-    if __name__ == "__main__":
-        save_image(self, 1, 1, "image_data")
-        on_post(self, 1, 200, 1)
+        req.context["result"] = {"status": {"code": 200, "message": "success"}}
