cae133c04843381002a05e7a4b17857b026ce4c1
michael fernando <michaelfernando784@gmail.com>
2018-04-07 10:50:37 +0700

    update.py revisi (#5)
    * Add files via upload
    
    * Add files via upload
    
    * Delete serviceAccountKey.json
    
    * Delete update.py
    
    * Delete Dockerfile
    
    * Delete README.md
    
    * Delete requirements.txt
    
    * Delete run.py
    
    * Update update.py
    
    * Add files via upload
    
    * Delete update.py
    
    * Add files via upload
    
    * Update update.py
    
    * Update update.py
    
    * Add files via upload
    
    * Add files via upload
    
    * Update kuis.py
    
    * Add files via upload
    
    * update display.py
    
    * Add files via upload
    
    * Update update.py
    
    * Add files via upload
    
    * Update kuis.py
    
    * Update display.py
    
    * Update play.py
    
    * Delete display.py
    
    * Delete kuis.py
    
    * Delete play.py
    
    * Add files via upload

--- run.py
+++ run.py
@@ -7,10 +7,11 @@ from lib.middleware.require_json import RequireJSON
 from lib.listener.play import PlayListener
 from lib.listener.kuis import KuisListener
 from lib.listener.display import DisplayListener
+from lib.listener.update import UpdateListener
 
 cors = CORS(allow_all_origins=True, allow_all_headers=True, allow_all_methods=True)
 app  = falcon.API(middleware=[
-  RequireJSON(), 
+  RequireJSON(),
   JSONTranslator(),
   cors.middleware
 ])
@@ -18,3 +19,4 @@ app  = falcon.API(middleware=[
 app.add_route("/kuis", KuisListener())
 app.add_route("/play/{kuis_id}", PlayListener())
 app.add_route("/display/{kuis_id}/{user_id}", DisplayListener())
+app.add_route("/update/{kuis_id}", UpdateListener())
