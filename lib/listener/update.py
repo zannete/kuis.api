4b7aa00f2e0fd40867edb3000d3e818a9c1865ec
root <root@gdgsurabaya.org>
2018-04-07 04:08:09 +0000

    fix tab issue

--- lib/listener/update.py
+++ lib/listener/update.py
@@ -17,22 +17,22 @@ class UpdateListener:
     profile_y      = doc["profileY"]
     profile_width  = doc["profileWidth"]
     profile_height = doc["profileHeight"]
-
-  try:
-    cred = credentials.Certificate(os.path.join(os.getcwd(), "serviceAccountKey.json"))
-    firebase_app = firebase_admin.initialize_app(cred, {
-      #'storageBucket': 'kuis.zannete.com',
-      'databaseURL': 'https://kuis-zannete.firebaseio.com/'
-    })
-  except ValueError:
-    print("Firebase has been initialized")
-
-  kuis_ref = db.reference("/kuis/{}".format(kuis_id))
-
-  kuis_ref.child("judul").set(judul)
-  kuis_ref.child("profileX").set(profile_x)
-  kuis_ref.child("profileY").set(profile_y)
-  kuis_ref.child("profileWidth").set(profile_width)
-  kuis_ref.child("profileHeight").set(profile_height)
-
-  req.context["result"] = {"status": {"code": 200, "message": "success"}}
+  
+    try:
+      cred = credentials.Certificate(os.path.join(os.getcwd(), "serviceAccountKey.json"))
+      firebase_app = firebase_admin.initialize_app(cred, {
+        #'storageBucket': 'kuis.zannete.com',
+        'databaseURL': 'https://kuis-zannete.firebaseio.com/'
+      })
+    except ValueError:
+      print("Firebase has been initialized")
+  
+    kuis_ref = db.reference("/kuis/{}".format(kuis_id))
+  
+    kuis_ref.child("judul").set(judul)
+    kuis_ref.child("profileX").set(profile_x)
+    kuis_ref.child("profileY").set(profile_y)
+    kuis_ref.child("profileWidth").set(profile_width)
+    kuis_ref.child("profileHeight").set(profile_height)
+  
+    req.context["result"] = {"status": {"code": 200, "message": "success"}}
