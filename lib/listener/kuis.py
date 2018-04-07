5f3dcd9ce7655610686e31e1c608c69a6b353e75
michael fernando <michaelfernando784@gmail.com>
2018-04-07 09:37:58 +0700

    Update kuis.py

--- lib/listener/kuis.py
+++ lib/listener/kuis.py
@@ -8,26 +8,25 @@ from firebase_admin import storage
 from firebase_admin import db
 
 class KuisListener:
+  def on_post(self, req, res):
+    doc            = req.context["doc"]
+    judul          = doc["judul"]
+    profile_x      = doc["profileX"]
+    profile_y      = doc["profileY"]
+    profile_width  = doc["profileWidth"]
+    profile_height = doc["profileHeight"]
 
-    def on_post(self, req, res):
-        doc            = req.context["doc"]
-        judul          = doc["judul"]
-        profile_x      = doc["profileX"]
-        profile_y      = doc["profileY"]
-        profile_width  = doc["profileWidth"]
-        profile_height = doc["profileHeight"]
-
-        try:
-            cred         = credentials.Certificate(os.path.join(os.getcwd(), "serviceAccountKey.json"))
-            firebase_app = firebase_admin.initialize_app(cred, {
-            'databaseURL': 'https://kuis-zannete.firebaseio.com/'
-          })
-        except ValueError:
-            print("Firebase has been initialized")
+    try:
+      cred         = credentials.Certificate(os.path.join(os.getcwd(), "serviceAccountKey.json"))
+      firebase_app = firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://kuis-zannete.firebaseio.com/'
+      })
+    except ValueError:
+      print("Firebase has been initialized")
 
 
-        kuis_ref     = db.reference('/kuis')
-        new_kuis_ref = kuis_ref.push()
-        new_kuis_ref.set(doc)
+    kuis_ref     = db.reference('/kuis')
+    new_kuis_ref = kuis_ref.push()
+    new_kuis_ref.set(doc)
 
-        req.context["result"] = {"status": {"code": 200, "message": "success"}}
+    req.context["result"] = {"status": {"code": 200, "message": "success"}}
